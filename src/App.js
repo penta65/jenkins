d85b2ffff62e7d2a0d4de293d46a88cddbf1e1c4
Muhammad Hassan <muhammadhassanmurtaza@gmail.com>
2022-10-08 00:45:38 +0500

    update 1

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          working
-        </p>
+       <h1>update 1</h1>
       </div>
     );
   }
